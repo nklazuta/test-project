f745c5ad71b4d6f238435b03a7068476d72c1594
nklazuta <nkgavshina@gmail.com>
2022-03-15 16:27:11 +0300

    изменить разметку компонента Steps

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -9,27 +9,33 @@ function Steps() {
     <section className="steps">
       <h2 className="steps__title">Join other investors in 3 easy steps</h2>
       <ul className="steps__cards">
-        <Card
-          number="1"
-          title="Create wallet"
-          text="Wallet allows you to securily connect and store tokens"
-          image={coins}
-          link="Create wallet"
-        />
-        <Card
-          number="2"
-          title="Get tokens"
-          text="Buy tokens on and recieve them on your wallet"
-          image={coins}
-          link="Buy tokens"
-        />
-        <Card
-          number="3"
-          title="Hold tokens"
-          text="By holding tokens you become eligeble to recive monthly payments"
-          image={piles}
-          link="How to recieve payments"
-        />
+        <li className="steps__card">
+          <p className="steps__card-number">1</p>
+          <p className="steps__card-title">Create wallet</p>
+          <p className="steps__card-text">
+            Wallet allows you to securily connect and store tokens
+          </p>
+          <img className="steps__card-photo" src={coins} alt="Blue coins" />
+          <p className="steps__card-link">Create wallet</p>
+        </li>
+        <li className="steps__card">
+          <p className="steps__card-number">2</p>
+          <p className="steps__card-title">Get tokens</p>
+          <p className="steps__card-text">
+            Buy tokens on and recieve them on your wallet
+          </p>
+          <img className="steps__card-photo" src={coins} alt="Blue coins" />
+          <p className="steps__card-link">Buy tokens</p>
+        </li>
+        <li className="steps__card">
+          <p className="steps__card-number">3</p>
+          <p className="steps__card-title">Hold tokens</p>
+          <p className="steps__card-text">
+            By holding tokens you become eligeble to recive monthly payments
+          </p>
+          <img className="steps__card-photo" src={piles} alt="Blue coins" />
+          <p className="steps__card-link">How to recieve payments</p>
+        </li>
       </ul>
     </section>
   );
