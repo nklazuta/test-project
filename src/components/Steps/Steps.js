940749184c1957e472197f24c46e1e9fb1796c6c
nklazuta <nkgavshina@gmail.com>
2022-03-15 22:30:24 +0300

    feat: все карточки одной высоты

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -32,36 +32,42 @@ function Steps() {
       <h2 className="steps__title">Join other investors in 3 easy steps</h2>
       <ul className="steps__cards">
         <li className="steps__card">
-          <p className="steps__card-number">1</p>
-          <p className="steps__card-title">Create wallet</p>
-          <p className="steps__card-text">
-            Wallet allows you to securily connect and store tokens
-          </p>
-          <img className="steps__card-photo" src={coins} alt="Blue coins" />
+          <div className="steps__card-box">
+            <p className="steps__card-number">1</p>
+            <p className="steps__card-title">Create wallet</p>
+            <p className="steps__card-text">
+              Wallet allows you to securily connect and store tokens
+            </p>
+            <img className="steps__card-photo" src={coins} alt="Blue coins" />
+          </div>
           <p className="steps__card-link">Create wallet</p>
         </li>
 
         <li className="steps__card">
-          <p className="steps__card-number">2</p>
-          <p className="steps__card-title">Get tokens</p>
-          <p className="steps__card-text steps__card-text_type_narrow">
-            Buy tokens on and recieve them on your wallet
-          </p>
-          <img className="steps__card-photo" src={coins} alt="Blue coins" />
+          <div className="steps__card-box">
+            <p className="steps__card-number">2</p>
+            <p className="steps__card-title">Get tokens</p>
+            <p className="steps__card-text steps__card-text_type_narrow">
+              Buy tokens on and recieve them on your wallet
+            </p>
+            <img className="steps__card-photo" src={coins} alt="Blue coins" />
+          </div>
           <p className="steps__card-link">Buy tokens</p>
         </li>
 
         <li className="steps__card">
-          <p className="steps__card-number">3</p>
-          <p className="steps__card-title">Hold tokens</p>
-          <p className="steps__card-text">
-            By holding tokens you become eligeble to recive monthly payments
-          </p>
-          <img
-            className="steps__card-photo steps__card-photo_type_piles"
-            src={piles}
-            alt="Blue coins"
-          />
+          <div className="steps__card-box">
+            <p className="steps__card-number">3</p>
+            <p className="steps__card-title">Hold tokens</p>
+            <p className="steps__card-text">
+              By holding tokens you become eligeble to recive monthly payments
+            </p>
+            <img
+              className="steps__card-photo steps__card-photo_type_piles"
+              src={piles}
+              alt="Blue coins"
+            />
+          </div>
           <p className="steps__card-link">{linkText}</p>
         </li>
       </ul>
