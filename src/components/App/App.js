8c7472f6edfabf543e748cc12fecf5cb3ab6ed21
N. Lazuta <nkgavshina@gmail.com>
2022-03-20 16:53:56 +0300

    refactor: добавить комментарии

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,14 +8,17 @@ import "./App.css";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
 
+  //Обработчик клика по кнопке, открывающей попап
   const handleOpenPopupButtonClick = () => {
     setIsOpen(true);
   };
 
-  const closePopup = (evt) => {
+  //Закрыть попап
+  const closePopup = () => {
     setIsOpen(false);
   };
 
+  //Обработчик сабмита формы
   const handleSubmit = ({ phone }) => {
     setIsOpen(false);
     console.log(phone);
